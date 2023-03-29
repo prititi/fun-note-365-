74eefb5a30e03de9c79a14739256633bed60f57f
prititi <pt446914@gmail.com>
2023-03-29 19:16:56 +0530

    productTour frontend completed

---
+++ Frontend/scripts/productTour.js
@@ -0,0 +1,22 @@
+let boxDropdown = document.querySelectorAll('.boxDropdown');
+for (let x = 0; x < document.querySelectorAll('li.categ').length; x++) {
+    let list = document.querySelectorAll('li.categ')[x];
+    let boxDropdown_list =  document.querySelectorAll('.boxDropdown')[x];
+    list.addEventListener('mouseenter',() =>{
+        boxDropdown_list.style.display = 'block';
+    })
+    list.addEventListener('mouseleave',() =>{
+        boxDropdown_list.style.display = 'none';
+    })
+}
+for (let x = 0; x < boxDropdown.length; x++) {
+    const box = boxDropdown[x];
+    box.addEventListener('mouseenter',() =>{
+        box.style.display = 'block';
+    })
+    box.addEventListener('mouseleave',() =>{
+        box.style.display = 'none';
+    })
+}
+
+
