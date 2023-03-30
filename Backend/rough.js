a907c51fd9f762bce633c4dd03c9da343c5ab525
Anandhu_P_A <anandhupa131@gmail.com>
2023-03-30 22:39:44 +0530

    day 3 work completed

---
+++ Backend/rough.js
@@ -0,0 +1,20 @@
+
+<!-- cdns start -->
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet"/>
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet"/>
+    <!-- MDB -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.css" rel="stylesheet" />
+    
+<!-- cdns end -->
+
+
+
+//js
+
+<!-- cdn start -->
+    <!-- MDB js -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.2.0/mdb.min.js" ></script>
+
+<!-- cdn end -->
