d66d0c852aa080557cc9c16ad4ae4304c6f95345
swapnil solanki <ssinghsolanki686@gmail.com>
2023-04-02 18:24:56 +0530

    Ranking admin page

--- Frontend/Rankadmin.js
+++ Frontend/Rankadmin.js
@@ -1,3 +1,26 @@
+let boxDropdown = document.querySelectorAll('.boxDropdown');
+for (let x = 0; x < document.querySelectorAll('li.categ').length; x++) {
+    let list = document.querySelectorAll('li.categ')[x];
+    let boxDropdown_list =  document.querySelectorAll('.boxDropdown')[x];
+    list.addEventListener('mouseenter',() =>{
+        boxDropdown_list.style.display = 'block';
+    })
+    list.addEventListener('mouseleave',() =>{
+        boxDropdown_list.style.display = 'none';
+    })
+}
+for (let x = 0; x < boxDropdown.length; x++) {
+    const box = boxDropdown[x];
+    box.addEventListener('mouseenter',() =>{
+        box.style.display = 'block';
+    })
+    box.addEventListener('mouseleave',() =>{
+        box.style.display = 'none';
+    })
+}
+
+
+
 const button = document.getElementById("submit");
 
 const socket = io("https://backend-wikn.onrender.com",{transports:["websocket"]});
@@ -11,8 +34,10 @@ const opt4 = document.getElementById("opt4").value;
 
     const obj = {room,question,opt1,opt2,opt3,opt4};
     console.log(obj);
-
     socket.emit("details",obj)
+    
+    setInterval(()=>{
+        window.location.href = "./index.html"
+    },1000)
 })
 
-console.log("Hello ")
